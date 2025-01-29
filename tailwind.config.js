72d0d27dd603bb7e3b912f8ce90ad90a98f10a06
siddikur2k22 <siddikur191285@gmail.com>
2025-01-29 21:17:38 +0600

    accordian added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        backgroundImage: {
+            'hero-pattern': "url('/images/hero-pattern.jpg')",
+            'footer-texture': "url('/images/footer-texture.png')",
+          },
+      },
     },
     plugins: [],
   }
